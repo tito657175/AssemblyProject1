2b1c56a1fac2e7655689ca34fee18c7860324bab
tito <tito657175>
2024-10-02 14:13:32 -0700

    Final working version of Phase 2 Figure 2.6

--- Figure2.6.cpp
+++ Figure2.6.cpp
@@ -2,12 +2,19 @@
 #include <iostream>
 
 int main(){
-    const int bonus = 10;
-    int exam1;
-    int exam2;
-    int score;
-    scanf("%d %d", &exam1, &exam2);
-    score = (exam1 + exam2) /2 + bonus;
-    printf("score = %d\n", score);
+    //const int bonus = 10;
+    int int1;
+    int int2;
+    int divide;
+    int remainder;
+    //Ask for two ints from user
+    printf("\nThis program will take two integers and divide them.\nEnter The First Integer: \n");
+    scanf("%d", &int1);
+    printf("Enter The Second Integer: \n");
+    scanf("%d", &int2);
+
+    divide = (int1 / int2);
+    remainder = (int1 % int2);
+    printf("Divide = %d  Remainder = %d\n", divide , remainder);
     return 0;
 }
