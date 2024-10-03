9ada9703635ada01a243292b9c427f481a4438cc
tito <tito657175>
2024-10-02 19:47:44 -0700

    Original Commit phase 3 fig 2.12

--- Phase 3/Figure2.12.cpp
+++ Phase 3/Figure2.12.cpp
@@ -2,19 +2,22 @@
 // October 2, 2024
 // Project 1
 // CMPSCI-122-77814
-// Phase 2 Figure 2.12
+// Phase 3 Figure 2.12
 
 #include <iostream>
 
 int main() {
     int guess;
-    printf("Pick a number 0..3: ");
+    printf("Pick a number 10..15: \n");
     scanf("%d", &guess);
+    
     switch (guess) {
-        case 0: printf("Not close\n"); break;
-        case 1: printf("Close\n"); break;
-        case 2: printf("Right on\n"); break;
-        case 3: printf("Too high\n");
+        case 10: printf("A\n"); break;
+        case 11: printf("B\n"); break;
+        case 12: printf("C\n"); break;
+        case 13: printf("D\n"); break;
+        case 14: printf("E\n"); break;
+        case 15: printf("F\n");
     }
     return 0;
 }
