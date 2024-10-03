519014ee45ab0ee8320dddc2a2d2642b324c5184
tito <tito657175>
2024-10-02 17:07:31 -0700

    Add figure 2.10 phase 2 base selection switch, Phase 2 complete.

--- Phase 2/Figure2.10.cpp
+++ Phase 2/Figure2.10.cpp
@@ -5,17 +5,34 @@
 // Phase 2 Figure 2.10
 
 
-//#include <stdio.h>
 #include <iostream>
 
 int main(){
-    const int limit = 100;
+    //All variable declarations
+    const int base2 = 2;
+    const int base3 = 3;
+    const int base8 = 8;
+    const int base10 = 10;
+    const int base16 = 16;
     int num;
+
+
+    //Collect user input base int
+    printf("Enter a number base 2, 3, 8, 10, or 16: \n");
     scanf("%d", &num);
-    if (num >= limit) {
-        printf("high\n");
+
+    if(num==base2 || num==base3 || num==base8 || num==base10 || num==base16){
+        switch (num) {
+            case 2: printf("Binary 0..1"); break;
+            case 3: printf("Tertiary 0..2"); break;
+            case 8: printf("Octal 0..7"); break;
+            case 10: printf("Decimal 0..9"); break;
+            case 16: printf("Hexidecimal 0..F");
+        }
     } else {
-        printf("low\n");
+        printf("You did not enter a base integer 2, 3, 8, 10, or 16.\nThis program will now self-destruct.\n<Insert that code here later>");
     }
+    printf("\n");
+
     return 0;
 }
