0f8840b7935a83f54ec74e51364248e5ba4125c9
tito <tito657175>
2024-10-02 20:02:02 -0700

    Phase 3 fig 2.12 complete

--- Phase 3/Figure2.12.cpp
+++ Phase 3/Figure2.12.cpp
@@ -8,16 +8,26 @@
 
 int main() {
     int guess;
-    printf("Pick a number 10..15: \n");
+    printf("Pick a number 0 ... 15: \n");
     scanf("%d", &guess);
-    
+
     switch (guess) {
-        case 10: printf("A\n"); break;
-        case 11: printf("B\n"); break;
-        case 12: printf("C\n"); break;
-        case 13: printf("D\n"); break;
-        case 14: printf("E\n"); break;
-        case 15: printf("F\n");
+        case 0: printf("%d decimal is 0 in Hex\n", guess); break;
+        case 1: printf("%d decimal is 1 in Hex\n", guess); break;
+        case 2: printf("%d decimal is 2 in Hex\n", guess); break;
+        case 3: printf("%d decimal is 3 in Hex\n", guess); break;
+        case 4: printf("%d decimal is 4 in Hex\n", guess); break;
+        case 5: printf("%d decimal is 5 in Hex\n", guess); break;
+        case 6: printf("%d decimal is 6 in Hex\n", guess); break;
+        case 7: printf("%d decimal is 7 in Hex\n", guess); break;
+        case 8: printf("%d decimal is 8 in Hex\n", guess); break;
+        case 9: printf("%d decimal is 9 in Hex\n", guess); break;
+        case 10: printf("%d decimal is A in Hex\n", guess); break;
+        case 11: printf("%d decimal is B in Hex\n", guess); break;
+        case 12: printf("%d decimal is C in Hex\n", guess); break;
+        case 13: printf("%d decimal is D in Hex\n", guess); break;
+        case 14: printf("%d decimal is E in Hex\n", guess); break;
+        case 15: printf("%d decimal is F in Hex\n", guess);
     }
     return 0;
 }
